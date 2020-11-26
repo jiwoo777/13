1f7b5c3bb2566156a6e310ba20ce4ad346a63575
jiwoo01 <gwangbok1004@sookmyung.ac.kr>
2020-11-26 17:10:50 +0900

    practice 3

--- main.c
+++ main.c
@@ -1,22 +1,75 @@
- #include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include "keywords.h"
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int is_whitespace(char c){
+	if( c == ' ' ||
+	    c == '\n' ||
+		c == '\r' ||
+		c == '\t'||
+		c == '(')
+		return 1;
+		
+	return 0;	
+}
 
-struct student{
-	int ID;//학번 
-	char name[100];//문자열로 이름 
-	double grade;//학점 
-};//와플틀 완성 
+int fget_word(FILE *fp, char* word){
+	char c;
+	int cnt;
+	
+	while((c=fgetc(fp))!= EOF) {
+		if(is_whitespace(c) == 0)
+		   break;
+	}	
+	
+	if(c == EOF)
+	   return 0;
+	   
+	// filling out the word
+	cnt =0;
+	word[cnt++] = c;
+	word[cnt] = '\0';
+	
+	while ( (word[cnt] = fgetc(fp)) != EOF) {
+		if(is_whitespace(word[cnt]) == 1)
+		{
+			word[cnt] = '\0';
+			break;
+		}
+		cnt++;
+	}   
+	return cnt;
+} 
+
 
 int main(int argc, char *argv[]) {
-	struct student s1 = {123456, "JIWOO", 4.3};
 	
-	s1.ID = 456123;
-	s1.grade = 3.0; 
+	FILE *fp;//파일 포인터 선언 
+	char filename[100];
+	char word[100];
 	
-	printf("ID : %d\n", s1.ID);
-	printf("ID : %s\n", s1.name);//문자열 
-	printf("ID : %f\n", s1.grade);//소수 
+	//file open
+	printf("input the file name : ");
+	scanf("%s", filename);
+	
+	if ((fp = fopen(filename, "r")) == NULL )
+	{
+		printf("input filename is invalid (%s)\n", filename);
+		return -1;
+	}
+	
+	//bring a word from fp
+	while(fget_word(fp, word) != 0){
+	       count_word(word);
+	}
+	print_word();
+	
+	//bring a word
+	
+	fclose(fp);
+	
 	return 0;
 }
+
+
